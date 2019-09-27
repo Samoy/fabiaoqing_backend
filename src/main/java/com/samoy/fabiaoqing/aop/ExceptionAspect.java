5e12539a42e7cbc0fdbd0654073f04cc9aec0dd9
Samoy <samoy_young@163.com>
2019-09-27 14:57:05 +0800

    使用了aop进行异常日志打印

---
+++ src/main/java/com/samoy/fabiaoqing/aop/ExceptionAspect.java
@@ -0,0 +1,34 @@
+package com.samoy.fabiaoqing.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.catalina.connector.RequestFacade;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.stereotype.Component;
+
+/**
+ * ExceptionAspect
+ *
+ * @author Samoy
+ * @date 2019/9/27
+ */
+@Component
+@Aspect
+@Slf4j
+public class ExceptionAspect {
+    @Pointcut("within(com.samoy.fabiaoqing.expection.GlobalExceptionHandler)")
+    public void exceptionAspect() {
+
+    }
+
+    @Around(value = "exceptionAspect()")
+    public Object around(ProceedingJoinPoint joinPoint) throws Throwable {
+        RequestFacade requestFacade = (RequestFacade) joinPoint.getArgs()[0];
+        Exception exception = (Exception) joinPoint.getArgs()[1];
+        log.error("在请求:{}时发生了异常:{}", requestFacade.getRequestURI(), exception.getLocalizedMessage());
+        return joinPoint.proceed();
+    }
+}
--- src/main/java/com/samoy/fabiaoqing/expection/GlobalExceptionHandler.java
+++ src/main/java/com/samoy/fabiaoqing/expection/GlobalExceptionHandler.java
@@ -6,7 +6,6 @@ import lombok.extern.log4j.Log4j2;
 import org.mybatis.spring.MyBatisSystemException;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.converter.HttpMessageNotReadableException;
-import org.springframework.validation.BeanPropertyBindingResult;
 import org.springframework.validation.BindException;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
@@ -41,7 +40,6 @@ public class GlobalExceptionHandler {
      */
     @ExceptionHandler(BusinessException.class)
     public ApiResult businessExceptionHandler(HttpServletRequest request, BusinessException exception) {
-        log.error("业务异常:{},请求是:{}", exception.getMessage(), request.getRequestURI());
         return ApiResult.failure(exception.getCode(), exception.getMessage());
     }
 
@@ -54,7 +52,6 @@ public class GlobalExceptionHandler {
      */
     @ExceptionHandler({ServletRequestBindingException.class, HttpMessageNotReadableException.class})
     public ApiResult paramExceptionHandler(HttpServletRequest request, Exception exception) {
-        log.error("参数异常:{},请求是:{}", exception.getMessage(), request.getRequestURI());
         if (exception instanceof MissingServletRequestParameterException || exception instanceof HttpMessageNotReadableException) {
             return ApiResult.failure(ResponseEnum.PARAM_NOT_PRESENT);
         }
@@ -63,13 +60,11 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(HttpMediaTypeNotSupportedException.class)
     public ApiResult mediaException(HttpServletRequest request, HttpMediaTypeNotSupportedException exception) {
-        log.error("请求类型异常:{},请求是:{}", exception.getMessage(), request.getRequestURI());
         return ApiResult.failure(ResponseEnum.UNSUPPORTED_HTTP_TYPE);
     }
 
     @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
     public ApiResult httpRequestMethodNotSupportedException(HttpServletRequest request, HttpRequestMethodNotSupportedException exception) {
-        log.error("请求方法异常:{},请求是:{}", exception.getMessage(), request.getRequestURI());
         return ApiResult.failure(ResponseEnum.UNSUPPORTED_HTTP_REQUEST);
     }
 
@@ -91,7 +86,6 @@ public class GlobalExceptionHandler {
         }
         List<FieldError> errors = Objects.requireNonNull(bindingResult).getFieldErrors();
         String messages = errors.stream().map(DefaultMessageSourceResolvable::getDefaultMessage).collect(Collectors.joining(","));
-        log.error("参数不合法:{},请求是:{}", messages, request.getRequestURI());
         return ApiResult.failure(ResponseEnum.PARAM_ILLEGAL.getCode(), messages);
     }
 
@@ -104,7 +98,6 @@ public class GlobalExceptionHandler {
      */
     @ExceptionHandler(RuntimeException.class)
     public ApiResult runtimeException(HttpServletRequest request, RuntimeException exception) {
-        log.error("运行时异常:{},请求是:{}", exception.getMessage(), request.getRequestURI());
         return ApiResult.failure(ResponseEnum.INTERNAL_ERROR);
     }
 
@@ -117,13 +110,11 @@ public class GlobalExceptionHandler {
      */
     @ExceptionHandler(MyBatisSystemException.class)
     public ApiResult dbExceptionHandler(HttpServletRequest request, MyBatisSystemException exception) {
-        log.error("数据库异常:{},请求是:{}", exception.getMessage(), request.getRequestURI());
         return ApiResult.failure(ResponseEnum.MYSQL_ERROR.getCode(), "服务器错误,请稍后再试");
     }
 
     @ExceptionHandler(Exception.class)
     public ApiResult commonException(HttpServletRequest request, Exception exception) {
-        log.error("未知异常:{},请求是:{}", exception.getMessage(), request.getRequestURI());
         return ApiResult.failure(ResponseEnum.UNKNOWN_ERROR);
     }
 }
